18ae7a732faef540c8980bf5056ce3d64953b72f
Guan <guanlin92@yahoo.com>
2016-04-13 14:07:40 -0700

    init commit

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ActiveRecord::Base
+
+	validates :name, presence: true
+	validates :street, presence: true
+	validates :city, presence: true
+	validates :state, presence: true
+	validates :zip, presence: true
+
+end
---
+++ app/views/restaurants/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @restaurant, :id, :name, :street, :city, :state, :zip, :phone, :website, :created_at, :updated_at
